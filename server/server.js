b303948b7ddcb849d488f6e93c583730a69a8576
Arambh Singh Rajput <arambhrajput1408@gmail.com>
2026-01-21 23:07:02 +0530

    Refactor server port configuration and logging

--- server/server.js
+++ server/server.js
@@ -7,9 +7,12 @@ const path = require("path");
 const app = express();
 app.use(express.static("client"));
 
-const server = app.listen(3000, () =>
-  console.log("Server running on http://localhost:3000")
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
 );
+
 
 const wss = new WebSocket.Server({ server });
 
